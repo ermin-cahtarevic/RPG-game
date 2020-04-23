88f3337001e968f390a87c83cf3fa7ff1afe5a95
Ermin Cahtarevic <erminc@windowslive.com>
2020-04-23 18:43:18 +0200

    Add basic file structure

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js',
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'index.html'),
+        to: path.resolve(__dirname, 'dist'),
+      },
+    ]),
+  ],
+};
