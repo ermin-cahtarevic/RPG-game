83756da095cd5effe93f7b89b5a990d1d02889a9
Ermin Cahtarevic <erminc@windowslive.com>
2020-04-27 16:28:01 +0200

    Clean up dist folder from unused assets

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -81,7 +81,7 @@ export default class PreloaderScene extends Phaser.Scene {
       this.ready();
     });
 
-    this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
+    // this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
     // ui assets
     this.load.image('forest-bg', 'assets/forest-bg.png');
